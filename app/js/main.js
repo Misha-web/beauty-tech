454c2bf407c424217f84f2547d2b4c679f838051
unknown <mirzoevmisha7@gmail.com>
2020-07-30 13:52:05 +0300

    site ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,19 @@
-$(function(){
-
-
-
-  
+let burgerMenu = document.querySelector('.burger__menu');
+let navMenu = document.querySelector('.nav__menu');
+let bodyLock = document.querySelector('body')
+burgerMenu.addEventListener("click", function() {
+    burgerMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    bodyLock.classList.toggle('lock');
 });
+
+window.onscroll = () => {
+    const navFixed = document.querySelector('.nav');
+    const navSctoll = window.scrollY;
+    if (navSctoll > 70) {
+        navFixed.classList.add('fixed');
+    } else if (navSctoll < 10) {
+        navFixed.classList.remove('fixed')
+    }
+}
+
